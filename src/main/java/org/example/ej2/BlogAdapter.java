99bca272b1ef02d613410bf51bfd6908a356721f
BertoSC <a23albertogc@iessanclemente.net>
2025-01-14 21:21:38 +0100

    prueba usando replace() para crear un nuevo index.html

--- src/main/java/org/example/ej2/BlogAdapter.java
+++ src/main/java/org/example/ej2/BlogAdapter.java
@@ -8,12 +8,12 @@ public class BlogAdapter implements JsonDeserializer<Blog> {
     @Override
     public Blog deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
         JsonObject jo = jsonElement.getAsJsonObject();
-        String content_html = jo.get("content_html").getAsString();
-        String title = jo.get("title").getAsString();
-        String photo_url= jo.get("photo_url").getAsString();
+        JsonObject joBlog = jo.getAsJsonObject("blog");
+        String content_html = joBlog.get("content_html").getAsString();
+        String title = joBlog.get("title").getAsString();
+        String photo_url= joBlog.get("photo_url").getAsString();
 
         Blog temp = new Blog(content_html, title, photo_url);
-
         return temp;
     }
 }
--- src/main/java/org/example/ej2/Main.java
+++ src/main/java/org/example/ej2/Main.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
@@ -12,16 +13,17 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
-    private static final String URL_LINK = "https://api.slingacademy.com/v1/sample-data/blog-posts/";
+    private static final String API_LINK = "https://api.slingacademy.com/v1/sample-data/blog-posts/";
 
     public static void main(String[] args) throws URISyntaxException, IOException {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduce un número de id:");
         int id = sc.nextInt();
-        URL peticion = new URI(URL_LINK+id).toURL();
+        URL peticion = new URI(API_LINK+id).toURL();
         HttpURLConnection con = (HttpURLConnection) peticion.openConnection();
         con.setRequestMethod("GET");
-        String json;
+        StringBuilder json = new StringBuilder();
         Gson gson = new GsonBuilder()
                 .setPrettyPrinting()
                 .registerTypeAdapter(Blog.class, new BlogAdapter()).create();
@@ -29,14 +31,20 @@ public class Main {
             try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()))){
                 String line;
                 while ((line=br.readLine())!=null){
-                    System.out.println(line);
-                    json = Files.readString(Paths.get("personaJson.json"));
-                    Blog temp = gson.fromJson(json, Blog.class);
-                    System.out.println(temp);
+                    json.append(line);
                 }
-
             }
         }
+        Blog temp = gson.fromJson(json.toString(), Blog.class);
+        System.out.println(temp);
+
+        String htmlArticle = Files.readString(Paths.get("src/main/resources/blog-resources/index.html"));
+        String nuevo = htmlArticle.replace( "<h2 class=\"title fs-1\"></h2>", "<h2 class=\"title fs-1\">"+temp.getTitle()+"</h2>");
+        nuevo = nuevo.replace("<!--<img src=\"IMAGE-SRC\" alt=\"article\">-->","<img src="+"\""+temp.getPhoto_url()+"\""+" alt=\"article\">"+temp.getContent_html());
+        System.out.println(nuevo);
+        File file = new File("src/main/resources/blog-resources/prueba.html");
+        Files.writeString(file.toPath(), nuevo);
+
 
     }
 }
